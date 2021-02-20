e729b4205b04a174737169f90da745cabc85dd27
Fernando Macedo <fernando.macedo.santana@gmail.com>
2021-02-20 18:10:22 -0300

    Solicitando dados de entrada das sondas

---
+++ src/ProgramaSonda.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ProgramaSonda {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] espacoTotal = scanner.nextLine().split(" ");
+
+        String[] primeiraSonda = scanner.nextLine().split(" ");
+
+        String[] comandosPrimeiraSonda = scanner.nextLine().split("");
+
+        String[] segundaSonda = scanner.nextLine().split(" ");
+
+        String[] comandosSegundaSonda = scanner.nextLine().split("");
+
+    }
+}
